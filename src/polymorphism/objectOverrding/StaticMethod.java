a10b304c533650680a2db563e0e339baa60726af
ajinkya111122 <nikhil908070@gmail.com>
2024-10-07 23:27:38 +0530

    new

---
+++ src/polymorphism/objectOverrding/StaticMethod.java
@@ -0,0 +1,14 @@
+package polymorphism.objectOverrding;
+
+public class StaticMethod {
+	
+	void display() {
+		System.out.println("hi");
+	}
+	
+	public static void main(String[] args) {
+		StaticMethod obj = null;
+		obj.display();
+	}
+
+}
